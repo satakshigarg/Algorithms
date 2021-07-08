97282c57421ae49bb30ee4bf97d4cff15e11187d
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-07-09 04:58:52 +0530

    Update bubbleSort.cpp

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -6,7 +6,7 @@ void swap(int a, int b) {
 }
 
 void bubbleSort(int *arr, int n) {
-    for (int i = (n-1); i >= 0; i++) {
+    for (int i = (n-1); i >= 0; i--) {
         for (int j = 0; j < i; j ++) {
             if (arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
