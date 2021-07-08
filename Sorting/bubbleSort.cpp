43c56d625db479838faa489cc048bcd559c8351c
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-07-09 04:55:26 +0530

    Create bubbleSort.cpp

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,16 @@
+void swap(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    return;
+}
+
+void bubbleSort(int *arr, int n) {
+    for (int i = (n-1); i >= 0; i++) {
+        for (int j = 0; j < i; j ++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
