537fa0d8bcc5ff358b1a95f4858bb2d4c59a99ee
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-07-20 14:07:01 -0700

    Create targetSumLeetCode.cpp

---
+++ DynamicProgramming/targetSumLeetCode.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int countSubsetSum(int* arr, int n, int sumArr, int target) {
+    
+    int sum = (target + sumArr)/2;
+    
+    vector<vector<int>> dp (n+1, vector<int>(sum+1, 0));
+    for (int i = 1; i <= n; i += 1) {
+        dp[i][0] = 1;
+    }
+    for (int j = 1; j <= sum; j += 1) {
+        dp[0][j] = 0;
+    }
+    
+    for (int i = 1; i <= n; i += 1) {
+        for (int j = 1; j <= sum; j += 1) {
+            if (arr[i-1] <= sum) {
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
+            }
+        }
+    }
+    
+    return dp[n][sum];
+    
+}
