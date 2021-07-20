951f522bb2bd92cba35ad4df09f4c94c3ae796da
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-07-20 14:19:57 -0700

    Update targetSumLeetCode.cpp

--- DynamicProgramming/targetSumLeetCode.cpp
+++ DynamicProgramming/targetSumLeetCode.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
-
-using namespace std;
-
-
-
-int countSubsetSum(int* arr, int n, int sumArr, int target) {
-    
-    int sum = (target + sumArr)/2;
-    
-    vector<vector<int>> dp (n+1, vector<int>(sum+1, 0));
-    for (int i = 1; i <= n; i += 1) {
-        dp[i][0] = 1;
-    }
-    for (int j = 1; j <= sum; j += 1) {
-        dp[0][j] = 0;
-    }
-    
-    for (int i = 1; i <= n; i += 1) {
-        for (int j = 1; j <= sum; j += 1) {
-            if (arr[i-1] <= sum) {
-                dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sumArr = 0;
+        for (auto n: nums) {
+            sumArr += n;
+        }
+        if ((sumArr+target) % 2 != 0) {
+            return 0;
+        }
+        int n = nums.size();
+        int sum = (sumArr+target)/2;
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
+        for (int i = 0; i <= n; i += 1) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i += 1) {
+            for (int j = 0; j <= sum; j += 1) {
+                if (nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
+        
+        
+        return dp[n][sum];
+        
+        
     }
-    
-    return dp[n][sum];
-    
-}
+};
