00c05c53681d8fad3f708131309599520d364d61
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-07-20 13:47:34 -0700

    Create countSubsetWithTargetSum1.cpp

---
+++ DynamicProgramming/countSubsetWithTargetSum1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int countSubsetSum(int* arr, int n, int sum) {
+    vector<vector<bool>> dp (n+1, vector<bool>(sum+1, false));
+    for (int i = 1; i <= n; i += 1) {
+        dp[i][0] = true;
+    }
+    for (int j = 1; j <= sum; j += 1) {
+        dp[0][j] = false;
+    }
+    
+    for (int i = 1; i <= n; i += 1) {
+        for (int j = 1; j <= sum; j += 1) {
+            dp[i][j] = dp[i-1][j];
+            if (arr[i-1] <= sum) {
+                dp[i][j] = dp[i-1][j-arr[i]];
+            }
+        }
+    }
+    int count = 0;
+    for (int i = 0; i <= n; i += 1) {
+        if (dp[i][sum]) {
+            count += 1;
+        }
+    }
+    return count;
+    
+}
